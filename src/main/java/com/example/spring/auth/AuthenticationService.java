43e2f770064cedbbdf2a6cc2277543803ba7351e
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-10-30 10:04:15 +0300

    Refresh-Token is done✔

--- src/main/java/com/example/spring/auth/AuthenticationService.java
+++ src/main/java/com/example/spring/auth/AuthenticationService.java
@@ -8,12 +8,19 @@ import com.example.spring.token.TokenType;
 import com.example.spring.user.Role;
 import com.example.spring.user.User;
 import com.example.spring.user.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -35,10 +42,31 @@ public class AuthenticationService {
        userRepository.save(user);
        var savedUser = userRepository.save(user);
        var jwtToken = jwtService.generateToken(user);
-        saveUserToken(user, jwtToken);
+       var refreshToken = jwtService.generateRefreshToken(user);
+        saveUserToken(savedUser, jwtToken);
         return  AuthenticationResponse
                 .builder()
-                .token(jwtToken)
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        revokeAllUserToken(user);
+        saveUserToken(user,jwtToken);
+        return  AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
                 .build();
     }
     public void revokeAllUserToken(User user){
@@ -61,20 +89,31 @@ public class AuthenticationService {
         tokenRepository.save(token);
     }
 
-    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                )
-        );
-        var user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow();
-        var jwtToken = jwtService.generateToken(user);
-        revokeAllUserToken(user);
-        saveUserToken(user,jwtToken);
-        return  AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+        final String authHeader= request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String userEmail;
+        if (authHeader==null || !authHeader.startsWith("Bearer "))
+        {
+            return;
+        }
+        refreshToken= authHeader.substring(7);
+        userEmail = jwtService.extractUsername(refreshToken);
+        if (userEmail !=null){
+            var user=this.userRepository.findByEmail(userEmail)
+                    .orElseThrow();
+            if (jwtService.isTokenValid(refreshToken,user)){
+                var accessToken = jwtService.generateToken(user);
+                revokeAllUserToken(user);
+                saveUserToken(user,accessToken);
+                var authResponse = AuthenticationResponse.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken)
+                        .build();
+                new ObjectMapper().writeValue(response.getOutputStream(),authResponse);
+             }
+        }
     }
 }
--- src/main/java/com/example/spring/config/JwtService.java
+++ src/main/java/com/example/spring/config/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,15 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
+    @Value("${jwt.secret-key}")
+    private  String SECRET_KEY ;
 
-    private static final String SECRET_KEY = "ec5f22a164d4fc21baa96b5568ec51e9778a2abce07df7eef3a4bf1520066596";
+    @Value("${jwt.expiration}")
+    private long jwtExpiration ;
+
+    @Value("${jwt.refresh-token-expiration}")
+    private long refreshTokenExpiration;
+
     public String extractUsername(String token) {
 
         return extractClaim(token,Claims::getSubject);
@@ -29,22 +37,34 @@ public class JwtService {
         return generateToken(new HashMap<>(),userDetails);
     }
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
 
     }
      public String generateToken(
              Map<String,Object> extraClaims,
-             UserDetails userDetails){
-        return Jwts.builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24*10))
-                .signWith(getSignKey(), SignatureAlgorithm.HS256)
-                .compact();
+             UserDetails userDetails) {
+        return buildToken(extraClaims,userDetails,jwtExpiration);
      }
+
+     public String generateRefreshToken(
+            UserDetails userDetails) {
+        return buildToken(new HashMap<>(),userDetails,refreshTokenExpiration);
+    }
+
+     public String buildToken(
+         Map<String,Object> extraClaims,
+         UserDetails userDetails,
+         long expiration){
+        return Jwts
+                .builder()
+             .setClaims(extraClaims)
+             .setSubject(userDetails.getUsername())
+             .setIssuedAt(new Date(System.currentTimeMillis()))
+             .setExpiration(new Date(System.currentTimeMillis() +expiration ))
+             .signWith(getSignKey(), SignatureAlgorithm.HS256)
+             .compact();
+    }
 
      public boolean isTokenValid(String token,UserDetails userDetails){
          final String username = extractUsername(token);
